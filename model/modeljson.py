09bd7dd5eb0e8470f6e392cdfbc3e9a6ecce6519
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2022-09-09 16:48:08 +0430

    Create modeljson.py

---
+++ model/modeljson.py
@@ -0,0 +1,8 @@
+registerjson= {
+    'type': 'object',
+    'properties': {
+        'username': {'type': 'string'},
+        'token': {'type': 'string'}
+    },
+    'required': ['token','username']
+}
